184dd96dd4c71269c5145ef087a7054254c8d5a1
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-08-29 13:45:06 +0700

    fix - intro spacing

--- src/components/Presentational/Home/Intro.tsx
+++ src/components/Presentational/Home/Intro.tsx
@@ -3,28 +3,26 @@ import clsx from "clsx";
 
 const Intro = () => {
 	return (
-		<section
-			id="intro"
-			className={clsx(
-				"bg-primaryBg dark:bg-primaryBgDark mx-auto max-w-2xl py-32 sm:py-48 lg:py-56"
-			)}
-		>
-			<div className={clsx("hidden sm:mb-6 sm:flex sm:justify-center")}>
-				<div className={clsx("Border-pixel")}>
-					<span className={clsx("Border-pixel-label text-xl")}>
-						I&apos;m Wahid Rizka Fathurrohman
-					</span>
-					<span className={clsx("dot-bottom-right-4")}></span>
-					<span className={clsx("dot-bottom-left-4")}></span>
+		<section id="intro" className={clsx("bg-primaryBg dark:bg-primaryBgDark")}>
+			<div className={clsx("mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 px-4")}>
+				<div className={clsx("flex justify-center mb-2")}>
+					<div className={clsx("Border-pixel")}>
+						<span className={clsx("Border-pixel-label text-xl")}>
+							Hi there!
+						</span>
+						<span className={clsx("dot-bottom-right-4")}></span>
+						<span className={clsx("dot-bottom-left-4")}></span>
+					</div>
 				</div>
-			</div>
 
-			<div className={clsx("text-center")}>
-				<Text size="300" shadow="large">
-					An individual identifying as a Software Engineer expresses a keen
-					interest in Full Stack Development and possesses a comprehensive
-					understanding of Front-end Development.
-				</Text>
+				<div className={clsx("text-center space-y-2 flex flex-col")}>
+					<Heading>Wahid Rizka Fathurrohman</Heading>
+					<Text weight="semibold" shadow="large">
+						An individual identifying as a Software Engineer expresses a keen
+						interest in Full Stack Development and possesses a comprehensive
+						understanding of Front-end Development.
+					</Text>
+				</div>
 			</div>
 		</section>
 	);
