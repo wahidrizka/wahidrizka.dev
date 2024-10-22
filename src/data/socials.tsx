f9effaee19b2ad23af1488f34d979dadb1d8c534
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-10-22 14:29:31 +0700

    Update socials.tsx

--- src/data/socials.tsx
+++ src/data/socials.tsx
@@ -1,4 +1,5 @@
 import { SocialTypes } from "@/types";
+import { SmileyIcon } from "@primer/styled-octicons";
 
 export const socials: SocialTypes[] = [
 	{
@@ -6,23 +7,27 @@ export const socials: SocialTypes[] = [
 		name: "GitHub",
 		url: "https://www.github.com/wahidrizka",
 		// icon: <GitHubIcon width={64} height={64} />,
+		icon: <SmileyIcon size={64} />,
 	},
 	{
 		id: 2,
 		name: "LinkedIn",
 		url: "https://www.linkedin.com/in/wahidrizka",
 		// icon: <LinkedInIcon width={64} height={64} />,
+		icon: <SmileyIcon size={64} />,
 	},
 	{
 		id: 3,
 		name: "Instagram",
 		url: "",
 		// icon: <InstagramIcon width={64} height={64} />,
+		icon: <SmileyIcon size={64} />,
 	},
 	{
 		id: 4,
 		name: "Email",
 		url: "",
 		// icon: <GmailIcon width={64} height={64} />,
+		icon: <SmileyIcon size={64} />,
 	},
 ];
