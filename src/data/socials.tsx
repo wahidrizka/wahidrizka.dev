1c0de9fe08730f915a88b0f9e64daa85d1f929e4
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-10-22 14:27:32 +0700

    fix: imports

--- src/data/socials.tsx
+++ src/data/socials.tsx
@@ -1,7 +1,3 @@
-import { GitHubIcon } from "@/components/icons/GitHubIcon";
-import { GmailIcon } from "@/components/icons/GmailIcon";
-import { InstagramIcon } from "@/components/icons/InstagramIcon";
-import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
 import { SocialTypes } from "@/types";
 
 export const socials: SocialTypes[] = [
@@ -9,24 +5,24 @@ export const socials: SocialTypes[] = [
 		id: 1,
 		name: "GitHub",
 		url: "https://www.github.com/wahidrizka",
-		icon: <GitHubIcon width={64} height={64} />,
+		// icon: <GitHubIcon width={64} height={64} />,
 	},
 	{
 		id: 2,
 		name: "LinkedIn",
 		url: "https://www.linkedin.com/in/wahidrizka",
-		icon: <LinkedInIcon width={64} height={64} />,
+		// icon: <LinkedInIcon width={64} height={64} />,
 	},
 	{
 		id: 3,
 		name: "Instagram",
 		url: "",
-		icon: <InstagramIcon width={64} height={64} />,
+		// icon: <InstagramIcon width={64} height={64} />,
 	},
 	{
 		id: 4,
 		name: "Email",
 		url: "",
-		icon: <GmailIcon width={64} height={64} />,
+		// icon: <GmailIcon width={64} height={64} />,
 	},
 ];
