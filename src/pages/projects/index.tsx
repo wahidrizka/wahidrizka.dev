9bf6cc977b7a14830b86cfb6ef95e3f29d06b918
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-10-27 20:32:33 +0700

    feat: projects page

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,17 @@
+import { SEO } from "@/components";
+import { AppLayout } from "@/layouts";
+
+export default function Projects() {
+	return (
+		<AppLayout>
+			<SEO
+				title="Wahid Rizka"
+				description="I am a Fullstack Developer with a strong interest in Software Engineering."
+			/>
+
+			<div data-color-mode="dark">
+				<h1>Coming Soon!</h1>
+			</div>
+		</AppLayout>
+	);
+}
